21d0dd0fea3fc93900d08aa7ce221157f3781131
andreselduro <104477633+andreselduro@users.noreply.github.com>
2022-04-27 21:25:43 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("ingrese un numero ");
+int numero = int.Parse(Console.ReadLine());
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine("es numero par");
+
+}
+else
+{
+    Console.WriteLine(" no es numero par");
+}
