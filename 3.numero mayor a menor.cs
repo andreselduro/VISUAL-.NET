3838bcdbfe8147740942df11104b447631f120ea
andreselduro <104477633+andreselduro@users.noreply.github.com>
2022-04-27 21:31:16 -0500

    Add files via upload

---
+++ 3.numero mayor a menor.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("ingrese numero 1");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("ingrese numero 2");
+    int num2 = int.Parse(Console.ReadLine());
+if ( num1 > num2)
+{
+    Console.WriteLine("el numero mayor es el numero 1");
+}
+else
+{
+    Console.WriteLine("el numero mayor es el numero 2");
+}
