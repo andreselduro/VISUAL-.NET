5cf86ed9668e1ce2f9c830b8978df850275e4bfd
andreselduro <104477633+andreselduro@users.noreply.github.com>
2022-04-27 21:30:26 -0500

    Add files via upload

---
+++ 2.Negativo positivo o cero.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("ingrese un numero");
+double  numero = double.Parse(Console.ReadLine());
+if (numero == 0)
+{
+    Console.WriteLine("neutro");
+}
+else if (numero > 0)
+{
+    Console.WriteLine("positivo");
+}
+else
+{
+    Console.WriteLine("negativo");
+}
