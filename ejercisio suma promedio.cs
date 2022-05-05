97dab24579e4ba8b1224551dd9308c17f1ed96e4
andreselduro <104477633+andreselduro@users.noreply.github.com>
2022-05-04 20:47:49 -0500

    Add files via upload

---
+++ ejercisio suma promedio.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("INGRESE 10 NUMEROS");
+int numero1 = int.Parse(Console.ReadLine());
+int numero2 = int.Parse(Console.ReadLine());
+int numero3 = int.Parse(Console.ReadLine());
+int numero4 = int.Parse(Console.ReadLine());
+int numero5 = int.Parse(Console.ReadLine());
+int numero6 = int.Parse(Console.ReadLine());
+int numero7 = int.Parse(Console.ReadLine());
+int numero8 = int.Parse(Console.ReadLine());
+int numero9 = int.Parse(Console.ReadLine());
+int numero10 = int.Parse(Console.ReadLine());
+int suma;
+int promedio;
+
+suma = numero1 + numero2 + numero3 + numero4 + numero5 + numero6 + numero7 + numero8 + numero9 + numero10;
+promedio = suma / 10;
+
+Console.WriteLine($"La suma de los numero es {suma} y su promedio {promedio}");
+    
